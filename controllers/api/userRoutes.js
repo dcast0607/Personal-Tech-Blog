ff6b9a1b118e969fff5ee0a245f6e9e20ee4fe91
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-09-11 10:36:16 -0700

    Added base logic for user creation and user login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -22,7 +22,22 @@ router.use((req, res, next) => {
 
 router.post('/', async (req, res) => {
     try {
+        const existingUser = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        })
+        if (existingUser) {
+            res.status(400).json("User already exists please try again!");
+        } else {
+            console.log(req.body);
+            const newUser = await User.create(req.body);
 
+            req.session.user_id = newUser.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(newUser);
+        };
     }
     catch (err) {
         res.status(400).json(err);
@@ -31,7 +46,35 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
+        console.log(req.body);
+        const existingUser = await User.findOne({
+            where: {
+                username: req.body.username,
+            }
+        })
+        .then((existingUser) => {
+            //console.log(existingUser);
+            if (existingUser) {
+                const passwordValidator = existingUser.checkUserPassword(req.body.password);
+                if (passwordValidator) {
 
+                    req.session.save(() => {
+                        req.session.user_id = existingUser.id;
+                        req.session.logged_in = true;
+                        res.status(200).json("User logged in successfully.");
+                    });
+
+                } else {
+                    res.status(400).json("Password does not match, please try again.");
+                }
+
+            } else {
+                res.status(400).json("We could not find the user, please make sure that you are attempting to login as an existing user.");
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
     }
     catch (err) {
         res.status(400).json(err);
